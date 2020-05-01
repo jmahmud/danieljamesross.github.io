fc8cc5f61c85531d26e7c06b96e3ffab17e4142a
Daniel J. Ross <mr.danielross@gmail.com>
2020-05-01 10:30:51 +0100

    Updates

---
+++ precache-manifest.d76407d6232edce9097adb023782f1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b669ec0bc2716e86500d6abcb6f55b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4b552609e5c8a28940",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3ad068aac00793681ec",
+    "url": "/static/js/2.f6d049d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f6d049d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4b552609e5c8a28940",
+    "url": "/static/js/main.482b92bf.chunk.js"
+  },
+  {
+    "revision": "91d5c726584fee2c72ec",
+    "url": "/static/js/runtime-main.62e81f4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
